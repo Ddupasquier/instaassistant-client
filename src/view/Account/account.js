b793511fefca84a5a6d69a385df736761b9733be
--replace-all <jonathonsmcclendon@gmail.com>
2022-07-14 19:20:45 -0500

    init

---
+++ src/view/Account/account.js
@@ -0,0 +1,20 @@
+const Account = () => {
+  return (
+    <>
+      <div>
+        <h1>@Username</h1>
+        <div className="row">
+          <div className="col-6">
+            <span id="account-info-1">Followers:</span>35.6K
+          </div>
+          <div className="col-6">
+            <span id="account-info-2">Interactions:</span>10K
+          </div>
+        </div>
+        <div className="row">
+          <div id="interaction-log" className="col-12"></div>
+        </div>
+      </div>
+    </>
+  );
+};
