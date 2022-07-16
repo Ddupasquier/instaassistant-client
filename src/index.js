14b5ffc95d710791427444e77f4b5db09a45e07b
--replace-all <jonathonsmcclendon@gmail.com>
2022-07-16 14:49:04 -0500

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './components/AppRoutes';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./components/AppRoutes";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
