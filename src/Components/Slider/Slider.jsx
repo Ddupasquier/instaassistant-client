5438b625625c3aca1af37af2467b872e24e2c6ff
--replace-all <jonathonsmcclendon@gmail.com>
2022-07-17 11:55:51 -0500

    added props

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './scss/slider-styles.css';
 
-function Slider() {
+function Slider(props) {
   const [slider, setSlider] = useState(false);
 
   const toggleRight = {
@@ -12,11 +12,10 @@ function Slider() {
   };
 
   return (
-    <button
+    <button id={props.name}
       className="slider"
       onClick={() => {
         setSlider(!slider);
-        console.log(slider);
       }}
       style={
         slider ? { backgroundColor: '#00FFFFFF' } : { backgroundColor: '#C0C2C9' }
--- src/View/Settings/Settings.jsx
+++ src/View/Settings/Settings.jsx
@@ -3,7 +3,7 @@ import Slider from '../../Components/Slider';
 import './scss/settings-styles.css';
 
 const Settings = () => {
-  return <div className="settings"><Slider /></div>;
+  return <div className="settings"><Slider name="follows" /><Slider name="likes" /><Slider name="dms" /><Slider name="comments" /></div>;
 };
 
 export default Settings;
