68ea4b67701f075c44966a640a3ebab766b79353
dylandupasquier <dylandupasquier@gmail.com>
2022-07-19 05:58:00 -0700

    onScreen

--- src/View/Account/Account.jsx
+++ src/View/Account/Account.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scss/account-styles.css';
-import Button from '../../Components/Button';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
+  const [onScreen, setOnScreen] = useState('');
+
+  let logText = 'your mom';
+
+  useEffect(() => {
+    setOnScreen(logText);
+  }, [logText]);
+
   return (
     <div className="account-container">
       <div className="head">
         <h3>@Username</h3>
       </div>
       <div className="account-buttons">
-        <Button text={'Back'} />
-        <Button text={'Edit'} />
+        <Link className="button outset" to="/accounts">
+          Back
+        </Link>
+        <Link className="button outset" to="">
+          Edit
+        </Link>
       </div>
       <div className="account-main">
         <div className="account-controls">
-          <div className="action-log">Action Log +</div>
-          <div className="controls">Add or GO</div>
+          <div className="action-log">
+            <b>Action Log</b> +
+          </div>
+          <div className="controls">
+            <b>ADD</b> or <b>GO</b>
+          </div>
         </div>
-        <div className="account-log inset"></div>
+        <div className="account-log inset">{onScreen}</div>
       </div>
     </div>
   );
