b19d03b3fce8c8591ce31eab4a0570795a7f3f80
dylandupasquier <dylandupasquier@gmail.com>
2022-07-19 05:34:59 -0700

    skeleton built

--- src/View/Account/Account.jsx
+++ src/View/Account/Account.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import AccountCard from '../../Components/AccountCard/AccountCard';
 import './scss/account-styles.css';
+import Button from '../../Components/Button';
 
 const Profile = () => {
   return (
-    <div className="account">
-      <AccountCard />
+    <div className="account-container">
+      <div className="head">
+        <h3>@Username</h3>
+      </div>
+      <div className="account-buttons">
+        <Button text={'Back'} />
+        <Button text={'Edit'} />
+      </div>
+      <div className="account-main">
+        <div className="account-controls">
+          <div className="action-log">Action Log +</div>
+          <div className="controls">Add or GO</div>
+        </div>
+        <div className="account-log inset"></div>
+      </div>
     </div>
   );
 };
