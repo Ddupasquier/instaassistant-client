7948a885f20fe59002095f69459f6193f4ab8a5b
dylandupasquier <dylandupasquier@gmail.com>
2022-07-19 17:57:45 -0700

    pushy

--- src/View/Account/Account.jsx
+++ src/View/Account/Account.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './scss/account-styles.css';
 import { Link } from 'react-router-dom';
 
-const Profile = () => {
+const Account = () => {
   const [onScreen, setOnScreen] = useState('');
 
   let logText = 'your mom';
