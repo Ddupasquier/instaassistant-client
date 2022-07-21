925a58c00b918eaeb967727d37cb186b212e46b2
dylandupasquier <dylandupasquier@gmail.com>
2022-07-20 18:45:38 -0700

    hmmmm

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BsGearWideConnected } from 'react-icons/bs';
 import { FaUserAlt, FaUserFriends } from 'react-icons/fa';
 import { VscGraphLine } from 'react-icons/vsc';
-import {MenuItem} from '../MenuItem';
+import { MenuItem } from '../MenuItem';
 import './scss/menu-styles.css';
 
 const menuItems = [
@@ -28,14 +28,39 @@ const menuItems = [
   },
 ];
 
-function Menu() {
+const Menu = () => {
+  const popOverStyle = {
+    width: '8rem',
+    translate: 'translate(-50%, -50%)',
+  };
+
+  // const isHover = (hover) => {
+  //   console.log(hover);
+  //   return hover;
+  // };
+
   return (
-    <div className="menu">
-      {menuItems.map((item) => (
-        <MenuItem key={item.to} item={item} />
-      ))}
-    </div>
+    <>
+      <div className="menu">
+        {menuItems.map((item) => (
+          <MenuItem key={item.to} item={item} 
+          // isHover={isHover} 
+          />
+        ))}
+      </div>
+      <div className="pop-overs">
+        {menuItems.map((item) => (
+          <div
+            key={item.to}
+            className="pop-over"
+            style={popOverStyle}
+          >
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default Menu;
--- src/Components/MenuItem/MenuItem.jsx
+++ src/Components/MenuItem/MenuItem.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const MenuItem = ({ item }) => {
+const MenuItem = ({ item, isHover }) => {
+  const [hover, setHover] = useState(false);
+//   isHover(hover);
+
   return (
-    <NavLink to={item.to} exact={true} activeClassName="active">
+    <NavLink
+      to={item.to}
+      exact="true"
+      activeClassName="active"
+      onMouseEnter={() => setHover(!hover)}
+      onMouseLeave={() => setHover(!hover)}
+    >
       <div className="menu-item ">
         <item.Icon />
-        {/* <span>{item.name}</span> */}
       </div>
     </NavLink>
   );
