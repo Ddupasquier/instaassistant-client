53a641f535a766ff6bf3d5fdf43e634fd6b8549c
dylandupasquier <dylandupasquier@gmail.com>
2022-07-23 12:58:28 -0700

    Add textarea component

--- src/View/instagramConfig/instagramConfig.jsx
+++ src/View/instagramConfig/instagramConfig.jsx
@@ -3,8 +3,27 @@ import { User, Button } from '@nextui-org/react';
 import './scss/instaconfig-styles.css';
 import { Link } from 'react-router-dom';
 import { Slider } from '../../Components/Slider';
+import { Textarea } from '../../Components/Textarea';
 
 function InstagramConfig() {
+  const textareas = [
+    {
+      legend: 'Look Alike',
+    },
+    {
+      legend: 'White List',
+    },
+    {
+      legend: 'Black List',
+    },
+    {
+      legend: 'Comments',
+    },
+    {
+      legend: 'Direct Messages',
+    },
+  ];
+
   return (
     <div className="insta-config">
       <div className="config-user">
@@ -35,41 +54,9 @@ function InstagramConfig() {
         </section>
       </div>
       <div className="config-textareas">
-        <div className="textarea-container">
-          <legend>Look-Alike Text Box</legend>
-          <br />
-          <textarea type="textarea" className="inset" />
-        </div>
-
-        <div className="textarea-container">
-          <legend>Black-List Text Box</legend>
-          <br />
-          <textarea type="textarea" className="inset" />
-        </div>
-
-        <div className="textarea-container">
-          <legend>White-List Text Box</legend>
-          <br />
-          <textarea type="textarea" className="inset" />
-        </div>
-
-        <div className="textarea-container">
-          <legend>Comments Text Box</legend>
-          <br />
-          <textarea type="textarea" className="inset" />
-        </div>
-
-        <div className="textarea-container">
-          <legend>Direct Messages Text Box</legend>
-          <br />
-          <textarea type="textarea" className="inset" />
-        </div>
-
-        <div className="textarea-container">
-          <legend>Comments Text Box</legend>
-          <br />
-          <textarea type="textarea" className="inset" />
-        </div>
+        {textareas.map((textarea) => (
+          <Textarea key={textarea.legend} legend={textarea.legend} />
+        ))}
       </div>
       <div className="config-buttons">
         <Button type="button" color="secondary" size="md" rounded>
