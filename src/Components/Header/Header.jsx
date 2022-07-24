fc88bfdea90cfa380eed93c9c89a6671a2f6b457
dylandupasquier <dylandupasquier@gmail.com>
2022-07-24 15:09:02 -0700

    aniamtion done!

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './scss/header-styles.css';
-
-const boxOne = {
-  top: '.6rem',
-  left: '.6rem',
-  borderRadius: '.1rem',
-  width: '1rem',
-  height: '1.5rem',
-};
 function Header({ billingSelected }) {
-  const [boxOneStyle, setBoxOneStyle] = useState(boxOne);
+  const [boxOneStyle, setBoxOneStyle] = useState({
+    top: '.6rem',
+    left: '.6rem',
+    borderRadius: '.1rem',
+    width: '1rem',
+    height: '1.5rem',
+  });
+  const [boxTwoStyle, setBoxTwoStyle] = useState({
+    top: '.8rem',
+    left: '.9rem',
+    borderRadius: '.1rem',
+    width: '1rem',
+    height: '1.5rem',
+  });
+  const [boxThreeStyle, setBoxThreeStyle] = useState({
+    top: '1.2rem',
+    left: '1.5rem',
+    borderRadius: '.1rem',
+    width: '1rem',
+    height: '1.5rem',
+  });
+  const [boxFourStyle, setBoxFourStyle] = useState({
+    top: '1rem',
+    left: '1.2rem',
+    borderRadius: '.1rem',
+    width: '1rem',
+    height: '1.5rem',
+  });
 
   useEffect(() => {
     const boxPositions = [
@@ -37,7 +56,7 @@ function Header({ billingSelected }) {
       },
       {
         top: ['55vh', '1.2rem'],
-        left: ['65vw', '1.5rem'],
+        left: ['60vw', '1.5rem'],
         borderRadius: ['.8rem', '.1rem'],
         width: ['10rem', '1rem'],
         height: ['15rem', '1.5rem'],
@@ -46,103 +65,81 @@ function Header({ billingSelected }) {
 
     const moveBoxOne = () => {
       if (billingSelected) {
-        let res = [];
+        let res = {};
         for (const rule in boxPositions[0]) {
-          res.push(` ${rule}: '${boxPositions[0][rule][0]}'`);
+          res[rule] = boxPositions[0][rule][0];
         }
         return res;
       } else {
-        let res = [];
+        let res = {};
         for (const rule in boxPositions[0]) {
-          res.push(` ${rule}: '${boxPositions[0][rule][1]}'`);
+          res[rule] = boxPositions[0][rule][1];
         }
         return res;
       }
     };
+
+    const moveBoxTwo = () => {
+      if (billingSelected) {
+        let res = {};
+        for (const rule in boxPositions[1]) {
+          res[rule] = boxPositions[1][rule][0];
+        }
+        return res;
+      } else {
+        let res = {};
+        for (const rule in boxPositions[1]) {
+          res[rule] = boxPositions[1][rule][1];
+        }
+        return res;
+      }
+    };
+
+    const moveBoxThree = () => {
+      if (billingSelected) {
+        let res = {};
+        for (const rule in boxPositions[2]) {
+          res[rule] = boxPositions[2][rule][0];
+        }
+        return res;
+      } else {
+        let res = {};
+        for (const rule in boxPositions[2]) {
+          res[rule] = boxPositions[2][rule][1];
+        }
+        return res;
+      }
+    };
+
+    const moveBoxFour = () => {
+      if (billingSelected) {
+        let res = {};
+        for (const rule in boxPositions[3]) {
+          res[rule] = boxPositions[3][rule][0];
+        }
+        return res;
+      } else {
+        let res = {};
+        for (const rule in boxPositions[3]) {
+          res[rule] = boxPositions[3][rule][1];
+        }
+        return res;
+      }
+    };
+
     setBoxOneStyle(moveBoxOne());
-
-    // const moveBoxTwo = () => {
-    //   if (billingSelected) {
-    //     let res = [];
-    //     for (const rule in boxPositions[1]) {
-    //       res.push(` ${rule}: '${boxPositions[1][rule][0]}'`);
-    //     }
-    //     return res;
-    //   } else {
-    //     let res = [];
-    //     for (const rule in boxPositions[1]) {
-    //       res.push(` ${rule}: '${boxPositions[1][rule][1]}'`);
-    //     }
-    //     return res;
-    //   }
-    // };
-
-    // const moveBoxThree = () => {
-    //   if (billingSelected) {
-    //     let res = [];
-    //     for (const rule in boxPositions[2]) {
-    //       res.push(` ${rule}: '${boxPositions[2][rule][0]}'`);
-    //     }
-    //     return res;
-    //   } else {
-    //     let res = [];
-    //     for (const rule in boxPositions[2]) {
-    //       res.push(` ${rule}: '${boxPositions[2][rule][1]}'`);
-    //     }
-    //     return res;
-    //   }
-    // };
-
-    // const moveBoxFour = () => {
-    //   if (billingSelected) {
-    //     let res = [];
-    //     for (const rule in boxPositions[3]) {
-    //       res.push(` ${rule}: '${boxPositions[3][rule][0]}'`);
-    //     }
-    //     return res;
-    //   } else {
-    //     let res = [];
-    //     for (const rule in boxPositions[3]) {
-    //       res.push(` ${rule}: '${boxPositions[3][rule][1]}'`);
-    //     }
-    //     return res;
-    //   }
-    // };
+    setBoxTwoStyle(moveBoxTwo());
+    setBoxThreeStyle(moveBoxThree());
+    setBoxFourStyle(moveBoxFour());
   }, [billingSelected]);
-
-  
-
-  const boxTwo = {
-    top: billingSelected ? '35vh' : '.8rem',
-    left: billingSelected ? '35vw' : '.9rem',
-    borderRadius: billingSelected ? '.8rem' : '.1rem',
-    width: billingSelected ? '10rem' : '1rem',
-    height: billingSelected ? '15rem' : '1.5rem',
-  };
-
-  const boxThree = {
-    top: billingSelected ? '50vh' : '1rem',
-    left: billingSelected ? '55vw' : '1.2rem',
-    borderRadius: billingSelected ? '.8rem' : '.1rem',
-    width: billingSelected ? '10rem' : '1rem',
-    height: billingSelected ? '15rem' : '1.5rem',
-  };
-
-  const boxFour = {
-    top: billingSelected ? '55vh' : '1.2rem',
-    left: billingSelected ? '60vw' : '1.5rem',
-    borderRadius: billingSelected ? '.8rem' : '.1rem',
-    width: billingSelected ? '10rem' : '1rem',
-    height: billingSelected ? '15rem' : '1.5rem',
-  };
 
   return (
     <header style={{ zIndex: billingSelected ? '1' : '1000' }}>
       <div className="logo">
         <div className="box-one" style={boxOneStyle} />
-        <div className="box-two" style={boxTwo} />
-        <div className="box-three" style={boxThree} />
-        <div className="box-four" style={boxFour} />
+        <div className="box-two" style={boxTwoStyle} />
+        <div className="box-three" style={boxThreeStyle} />
+        <div className="box-four" style={boxFourStyle} />
       </div>
       <div className="site-name">
         <i>Instant</i>
