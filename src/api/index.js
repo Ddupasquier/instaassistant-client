d6329c8b714c4ad31da4360439a6e10e8e081c3b
--replace-all <jonathonsmcclendon@gmail.com>
2022-07-24 17:16:11 -0500

    api layer function manual merge

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,45 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable quotes */
 /* eslint-disable arrow-body-style */
-import { InstagramTaskTypes } from "./endpoints";
+
+import {
+  InstagramTaskTypes,
+  UserViewPath,
+  UserLoginPath,
+  CreateAccountPath,
+  BotShowPath,
+  BotDeletePAth,
+} from "./endpoints";
 
 // get playlistList
 export const FetchInstagramTaskTypes = () => {
   return fetch(InstagramTaskTypes).then((response) => response.json());
 };
 
-export const FetchInstagramTaskTypes1 = () => {
-  return fetch(InstagramTaskTypes).then((response) => response.json());
+export const loginFetch = () => {
+  return fetch(UserLoginPath).then((response) => response.json());
+};
+
+export const GetUserInfo = () => {
+  return fetch(UserViewPath).then((response) => response.json());
+};
+
+export const CreateUserPost = () => {
+  return fetch(CreateAccountPath).then((response) => response.json());
+};
+
+export const GetBotInfo = () => {
+  return fetch(BotShowPath).then((response) => response.json());
+};
+
+export const CreateBot = (arg) => {
+  return fetch(BotShowPath[0] + arg + BotShowPath[1]).then((response) =>
+    response.json()
+  );
+};
+
+export const DeleteBot = () => {
+  return fetch(BotDeletePAth).then((response) => response.json());
 };
