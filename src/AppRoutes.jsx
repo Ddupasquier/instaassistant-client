d8f78d475ce97b7dd9c642d1efeb420b008e14d9
dylandupasquier <dylandupasquier@gmail.com>
2022-08-03 11:48:09 -0700

    background is in!

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -7,19 +7,37 @@ import { Billing } from './View/Billing';
 import { Profile } from './View/Profile';
 import { Account } from './View/Account';
 import { Accounts } from './View/Accounts';
-import {FAQ} from './View/FAQ';
+import { FAQ } from './View/FAQ';
 import { InstagramConfig } from './View/instagramConfig';
 import { NextUI } from './View/NextUI';
 import { Login } from './View/Login';
 import { SignUp } from './View/SignUp';
-
-// NextUI import
-import { NextUIProvider, createTheme, useDarkMode } from '@nextui-org/react';
 import { Task } from './View/Task';
 import Tasks from './View/Tasks';
 
+// NextUI import
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+
+
 const darkTheme = createTheme({
   type: 'dark',
+  theme: {
+    colors: {
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+      gradient:
+        'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      backgroundImage: 'url(https://i.ibb.co/KFXV3g0/abstract-lines-1.png)',
+    },
+  },
 });
 
 const lightTheme = createTheme({
