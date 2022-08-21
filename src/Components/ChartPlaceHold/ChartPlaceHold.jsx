0ec6e6fb2ce6b67518436723574c4ccc8ef1b136
--replace-all <jonathonsmcclendon@gmail.com>
2022-08-21 10:20:36 -0500

    factored state out of component and into parent

--- src/Components/ChartPlaceHold/ChartPlaceHold.jsx
+++ src/Components/ChartPlaceHold/ChartPlaceHold.jsx
@@ -10,61 +10,14 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const ChartPlaceHold = (obj) => {
 
-export default class ChartPlaceHold extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
-
-  render() {
     return (
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={500}
           height={300}
-          data={data}
+          data={obj}
           margin={{
             top: 5,
             right: 30,
@@ -75,17 +28,19 @@ export default class ChartPlaceHold extends PureComponent {
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
-          <Tooltip />
+          <Tooltip /> 
           <Legend />
           <Line
             type="monotone"
-            dataKey="pv"
+            dataKey="followers"
             stroke="#8884d8"
             activeDot={{ r: 8 }}
           />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="following" stroke="#82ca9d" />
         </LineChart>
       </ResponsiveContainer>
     );
-  }
 }
+
+export default ChartPlaceHold
+
