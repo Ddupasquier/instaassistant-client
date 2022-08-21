321ffdc0082c3a7a7528b4d7c423e8b6314d87bb
--replace-all <jonathonsmcclendon@gmail.com>
2022-08-21 10:19:38 -0500

    added development toggle for path while working localy

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,31 +1,29 @@
 //* User Routes
-export const CreateUserPath = "https://instaassistant-api.herokuapp.com/users";
-export const UserLoginPath = "https://instaassistant-api.herokuapp.com/login";
-export const UserViewPath = "https://instaassistant-api.herokuapp.com/users/";
+
+const local = true;
+const baseUrl = local
+  ? "http://localhost:3000"
+  : "https://instaassistant-api.herokuapp.com";
+
+export const CreateUserPath = baseUrl + "/users";
+export const UserLoginPath = baseUrl + "/login";
+export const UserViewPath = baseUrl + "/users/";
 
 //* START ACCOUNT
+export const CreateAccountPath = [baseUrl + "/users/", "/accounts"];
 
-export const BotCreatePath = [
-  "https://instaassistant-api.herokuapp.com/users/",
-  "/accounts",
-];
+export const AccountPatchPath = baseUrl + "/accounts/";
 
-export const AccountPatchPath =
-  "https://instaassistant-api.herokuapp.com/accounts/";
+export const AccountShowPath = baseUrl + "/accounts/";
 
-export const AccountShowPath =
-  "https://instaassistant-api.herokuapp.com/accounts/";
+export const BotIndexPath = baseUrl + "/user_accounts";
 
-export const BotIndexPath = "https://instaassistant-api.herokuapp.com/accounts";
-
-export const BotDeletePAth =
-  "https://instaassistant-api.herokuapp.com/accounts/"; // Delete bot based on bot_id
+export const BotDeletePAth = baseUrl + "/accounts/"; // Delete bot based on bot_id
 
 //* START Task for bot to complete
-export const TaskCreatePath = "https://instaassistant-api.herokuapp.com/tasks";
-export const TaskDeletePath = "https://instaassistant-api.herokuapp.com/tasks/";
+export const TaskCreatePath = baseUrl + "/tasks";
+export const TaskDeletePath = baseUrl + "/tasks/";
 
 //* START STATIC
-export const InstagramTaskTypes =
-  "https://instaassistant-api.herokuapp.com/task_type";
-export const faqPath = "https://instaassistant-api.herokuapp.com/faq";
+export const InstagramTaskTypes = baseUrl + "/task_type";
+export const faqPath = baseUrl + "/faq";
