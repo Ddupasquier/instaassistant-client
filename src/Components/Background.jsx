37e5502bc02baeb03e7df38cf50c4c09f287e650
--replace-all <jonathonsmcclendon@gmail.com>
2022-08-26 10:13:50 -0500

    changed bg

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Background = () => {
   const bgStyle = {
     backgroundImage:
-      "url('https://advancedwebtechnology.com/Portals/0/Images/ezgif-1-6ab989b982.jpeg?ver=FxjLZgyYJvyNBHO8IGcWmQ%3d%3d&timestamp=1661282809029')",
+      "url('https://advancedwebtechnology.com/Portals/0/Images/BGs4MarcusBot.png?ver=Y7bZZvZ8zbXtB9xvK0X74Q%3d%3d&timestamp=1661472043066')",
     backgroundPosition: 'center right',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
