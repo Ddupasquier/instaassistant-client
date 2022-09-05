e8547d50c5f8345f81ac15ed3d81e88134704f13
dylandupasquier <dylandupasquier@gmail.com>
2022-09-05 15:47:09 -0700

    temp billing

--- src/View/Billing/Billing.jsx
+++ src/View/Billing/Billing.jsx
@@ -1,38 +1,54 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { Card, Text } from '@nextui-org/react';
 import './scss/billing-styles.css';
 import PayPal from './PayPal';
 
 function Billing() {
-  // const [isChecked, setIsChecked] = useState(false);
-
   return (
     <div className="view-container">
-      <Card style={{zIndex: 1}} css={{background: '$myColor'}}>
-      <div className="head">
-          <Text
-            h1
-            size={60}
-            css={{
-              // textGradient: '45deg, $blue600 -20%, $pink600 50%',
-              color: 'white',
-            }}
-            weight="bold"
-          >
-            PAYMENT SETTINGS
+      <Card
+        style={{ zIndex: 1 }}
+        css={{
+          background: '$myColor',
+          width: '80%',
+          backdropFilter: 'blur(15px)',
+        }}
+      >
+        <div className="head">
+          <Text h1 size={50} css={{ color: 'white' }} weight="bold">
+            OUR PACKAGES
           </Text>
-        <Text h4>
-          Marcus Bot does not save any credit card information. <br />
-          We bill through paypal or through the secure third party Stripe.
-        </Text>
-      </div>
-      <br />
-      <div className="billing-main">
-        <PayPal num={1}/>
-        <PayPal num={5}/>
-        <PayPal num={10}/>
-      </div>
+          <Text h4>
+            Marcus Bot does not save any credit card information. <br />
+            We bill through paypal or through the secure third party Stripe.
+          </Text>
+        </div>
+        <Card.Body
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+          }}
+        >
+          <div className="pkg-item" style={{ background: '$myColor' }}>
+            <Text size={20} h5>
+              Professional Plan
+              <br /> $49 /mo*
+            </Text>
+          </div>
+          <div className="pkg-item" style={{ background: '$myColor' }}>
+            <Text size={20} h5>
+              Team Plan
+              <br /> $179 /mo*
+            </Text>
+          </div>
+          <div className="pkg-item" style={{ background: '$myColor' }}>
+            <Text size={20} h5>
+              Enterprise Plan
+              <br /> Contact for pricing
+            </Text>
+          </div>
+        </Card.Body>
       </Card>
     </div>
   );
