d6b320e11ed88a258a9a97dec84292c69278e3f3
dylandupasquier <dylandupasquier@gmail.com>
2022-09-05 16:08:28 -0700

    billing mocked up

--- src/View/Billing/Billing.jsx
+++ src/View/Billing/Billing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Text } from '@nextui-org/react';
+import { Card, Text, Spacer } from '@nextui-org/react';
 import './scss/billing-styles.css';
 import PayPal from './PayPal';
 
@@ -33,19 +33,64 @@ function Billing() {
           <div className="pkg-item" style={{ background: '$myColor' }}>
             <Text size={20} h5>
               Professional Plan
-              <br /> $49 /mo*
+              <br />
+              $49 /mo*
+            </Text>
+            <Text size={18} h5>
+              1 User
+              <br />
+              10 Social Accounts
+            </Text>
+            <Text size={15} h5>
+              $500 ad spend for social boosts Discover the best time to publish
+              Access messages in one inbox Schedule in bulk Access to free
+              integrations Live in-dash chat support
+            </Text>
+            <Spacer />
+            <Text size={12} h5>
+              Cancel Anytime.
             </Text>
           </div>
           <div className="pkg-item" style={{ background: '$myColor' }}>
             <Text size={20} h5>
               Team Plan
-              <br /> $179 /mo*
+              <br />
+              $179 /mo*
+            </Text>
+            <Text size={18} h5>
+              3 User
+              <br />
+              20 Social Accounts
+            </Text>
+            <Text size={15} h5>
+              $2000 ad spend for social boosts Analytics & reports Access
+              messages in one inbox Schedule in bulk Access to free integrations
+              Live in-dash chat support
+            </Text>
+            <Spacer />
+            <Text size={12} h5>
+              Cancel Anytime.
             </Text>
           </div>
           <div className="pkg-item" style={{ background: '$myColor' }}>
             <Text size={20} h5>
               Enterprise Plan
-              <br /> Contact for pricing
+              <br />
+              Contact for pricing
+            </Text>
+            <Text size={18} h5>
+              5+ User
+              <br />
+              50 Social Accounts
+            </Text>
+            <Text size={15} h5>
+              Publishing approvals Employee advocacy Social advertising Social
+              customer care App directory with 150+ tools Priority support and
+              training
+            </Text>
+            <Spacer />
+            <Text size={12} h5>
+              Cancel Anytime.
             </Text>
           </div>
         </Card.Body>
