fc2e6c1f4bee72ff49deb5b7b2365faeb1750de2
dylandupasquier <dylandupasquier@gmail.com>
2022-09-06 17:27:31 -0700

    recolor config tab

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Background = ({ theme, darkTheme }) => {
+const Background = () => {
   const bgStyle = {
     backgroundImage:
       "url('https://advancedwebtechnology.com/Portals/0/Images/GausInsta.png?ver=RCPqVbsbg8bkwZiB7z_lgw%3d%3d&timestamp=1661824105768')",
@@ -11,8 +11,6 @@ const Background = ({ theme, darkTheme }) => {
     bottom: '0',
     height: '100vh',
     width: '100vw',
-    // filter:
-    //   theme === darkTheme ? 'blur(1px) invert(0)' : 'blur(1px) invert(100%)',
   };
 
   return <div style={bgStyle} />;
