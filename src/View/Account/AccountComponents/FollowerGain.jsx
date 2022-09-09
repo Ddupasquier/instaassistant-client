5b32e27930a9d99d6db6fb7f0f62a62230799017
--replace-all <jonathonsmcclendon@gmail.com>
2022-09-08 20:21:49 -0500

    more tooltips added

--- src/View/Account/AccountComponents/FollowerGain.jsx
+++ src/View/Account/AccountComponents/FollowerGain.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Grid, Card, Text} from '@nextui-org/react';
+import { IconsQuestionMark } from 'Components/icons/icons';
 
 function FollowerGain({num}) {
       return ( <Grid sm={4} xs={12}>
@@ -9,7 +10,10 @@ function FollowerGain({num}) {
                 background: '$myColor',
               }}
             >
-              <Card.Header>Follower Gain - 30 Days</Card.Header>
+              <Card.Header>Follower Gain&nbsp;<IconsQuestionMark
+                    content="Followers gained over the specified time"
+                    local="top"
+                  /></Card.Header>
               <Card.Divider />
               <Card.Body>
                 <Text h2>{num}</Text>
