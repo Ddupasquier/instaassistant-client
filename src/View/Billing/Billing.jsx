330ec963121f67dfc619b4324200cc8297b2ba9a
dylandupasquier <dylandupasquier@gmail.com>
2022-09-13 17:53:53 -0700

    create package component, floar right 'cancel' txt

--- src/View/Billing/Billing.jsx
+++ src/View/Billing/Billing.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Card, Text, Spacer, Button } from '@nextui-org/react';
+import { Card, Text } from '@nextui-org/react';
 import './scss/billing-styles.css';
 import { CreateCheckoutSession } from 'api';
+import Package from './Package';
 
 function Billing() {
   const handleSubmit = (e) => {
@@ -80,99 +81,9 @@ function Billing() {
             justifyContent: 'space-around',
           }}
         >
-          <div className="pkg-item" style={{ background: '$myColor' }}>
-            <Text size={20} h5>
-              Professional Plan
-              <br />
-              $49 /mo*
-            </Text>
-            <Text size={18} h5>
-              1 User
-              <br />
-              10 Social Accounts
-            </Text>
-            <Text size={15} h5>
-              $500 ad spend for social boosts Discover the best time to publish
-              Access messages in one inbox Schedule in bulk Access to free
-              integrations Live in-dash chat support
-            </Text>
-            <Spacer />
-            <form onSubmit={handleSubmit}>
-              <input
-                type="hidden"
-                name="priceId"
-                value="price_G0FvDp6vZvdwRZ"
-              />
-              <Button style={{ zIndex: 1, float: 'right' }} type="submit">
-                Checkout
-              </Button>
-            </form>
-            <Text size={12} h5 color="red">
-              Cancel Anytime.
-            </Text>
-          </div>
-          <div className="pkg-item" style={{ background: '$myColor' }}>
-            <Text size={20} h5>
-              Team Plan
-              <br />
-              $179 /mo*
-            </Text>
-            <Text size={18} h5>
-              3 User
-              <br />
-              20 Social Accounts
-            </Text>
-            <Text size={15} h5>
-              $2000 ad spend for social boosts Analytics & reports Access
-              messages in one inbox Schedule in bulk Access to free integrations
-              Live in-dash chat support
-            </Text>
-            <Spacer />
-            <form onSubmit={handleSubmit}>
-              <input
-                type="hidden"
-                name="priceId"
-                value="price_G0FvDp6vZvdwRZ"
-              />
-              <Button style={{ zIndex: 1, float: 'right' }} type="submit">
-                Checkout
-              </Button>
-            </form>
-            <Text size={12} h5 color="red">
-              Cancel Anytime.
-            </Text>
-          </div>
-          <div className="pkg-item" style={{ background: '$myColor' }}>
-            <Text size={20} h5>
-              Enterprise Plan
-              <br />
-              Contact for pricing
-            </Text>
-            <Text size={18} h5>
-              5+ User
-              <br />
-              50 Social Accounts
-            </Text>
-            <Text size={15} h5>
-              Publishing approvals Employee advocacy Social advertising Social
-              customer care App directory with 150+ tools Priority support and
-              training
-            </Text>
-            <Spacer />
-            <form onSubmit={handleSubmit}>
-              <input
-                type="hidden"
-                name="priceId"
-                value="price_G0FvDp6vZvdwRZ"
-              />
-              <Button style={{ zIndex: 1, float: 'right' }} type="submit">
-                Checkout
-              </Button>
-            </form>
-            <Text size={12} h5 color="red">
-              Cancel Anytime.
-            </Text>
-          </div>
+          {packages.map((pkg) => (
+            <Package {...pkg} handleSubmit={handleSubmit} />
+          ))}
         </Card.Body>
       </Card>
     </div>
