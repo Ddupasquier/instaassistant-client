0be440467e95a7af6afb993d0252bb36fb0ef36f
dylandupasquier <dylandupasquier@gmail.com>
2022-11-17 07:09:21 -0800

    add twitter icons to platforms

--- src/platformIcons.js
+++ src/platformIcons.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import { FiInstagram } from 'react-icons/fi';
+import { FiInstagram, FiTwitter } from 'react-icons/fi';
 import { IoLogoYoutube } from 'react-icons/io';
 import { IoLogoTiktok } from 'react-icons/io5';
 
 export const platformIcon = (platform) => {
-  if (platform === 'INSTAGRAM') {
-    return <FiInstagram size="20" />;
-  } else if (platform === 'YOUTUBE') {
-    return <IoLogoYoutube size="20" />;
-  } else if (platform === 'TIKTOK') {
-    return <IoLogoTiktok size="20" />;
-  } else {
-    return <FiInstagram size="20" />;
+  switch (platform) {
+    case 'INSTAGRAM':
+      return <FiInstagram size="20" />;
+    case 'YOUTUBE':
+      return <IoLogoYoutube size="20" />;
+    case 'TIKTOK':
+      return <IoLogoTiktok size="20" />;
+    case 'TWITTER':
+      return <FiTwitter size="20" />;
+    default:
+      return <FiInstagram size="20" />;
   }
 };
