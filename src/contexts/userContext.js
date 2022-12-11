c7c6d466268c1772f58d7d15a3d867e3fd3f5e42
dylandupasquier <dylandupasquier@gmail.com>
2022-12-11 10:42:35 -0800

    create user context for profile info

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -1,22 +1,27 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { indexAccounts } from 'api';
+import { indexAccounts, GetUserInfo } from 'api';
 
 const UserContext = createContext(false);
 
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
   const [accounts, setAccounts] = useState(null);
 
   useEffect(() => {
     const getUser = async () => {
-      const response = await indexAccounts();
-      setAccounts(response);
+      const [userInfo, accounts] = await Promise.all([
+        GetUserInfo(),
+        indexAccounts(),
+      ]);
+      setUserInfo(userInfo);
+      setAccounts(accounts);
     };
     getUser();
   }, [user]);
 
   return (
-    <UserContext.Provider value={{ user, setUser, accounts }}>
+    <UserContext.Provider value={{ user, setUser, accounts, userInfo }}>
       {children}
     </UserContext.Provider>
   );
